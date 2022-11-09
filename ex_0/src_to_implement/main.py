c7066ef0e4ed1c32a705231cb8014a74270b94ae
Badiuzzaman Pranto <pranto@Badiuzzamans-MacBook-Air.local>
2022-11-09 17:13:06 +0100

    file path

--- ex_0/src_to_implement/main.py
+++ ex_0/src_to_implement/main.py
@@ -7,24 +7,24 @@ from generator import ImageGenerator
 # Other imports
 
 # Codes Here
-checker = Checker(250, 25)
-checker.draw()
-checker.show()
+# checker = Checker(250, 25)
+# checker.draw()
+# checker.show()
 
-circle = Circle(1024, 200, (512, 256))
-circle.draw()
-circle.show()
+# circle = Circle(1024, 200, (512, 256))
+# circle.draw()
+# circle.show()
 
-spectrum = Spectrum(10)
-spectrum.draw()
-spectrum.show()
+# spectrum = Spectrum(10)
+# spectrum.draw()
+# spectrum.show()
 
 img_gen = ImageGenerator(
-    file_path="/Users/admin/Desktop/DL_EXCERCISES/ex_0/src_to_implement/exercise_data",
-    label_path="/Users/admin/Desktop/DL_EXCERCISES/ex_0/src_to_implement/Labels.json",
+    file_path="/Users/pranto/Desktop/dl-exercises-fau/ex_0/src_to_implement/exercise_data",
+    label_path="/Users/pranto/Desktop/dl-exercises-fau/ex_0/src_to_implement/Labels.json",
     shuffle=True,
-    rotation=True,
-    mirroring=True,
+    rotation=False,
+    mirroring=False,
     batch_size=12,
     image_size=[100, 100, 3],
 )
