5d87b034b4f2eb33a7dc6a73725f66279441004c
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2022-11-22 11:16:48 +0100

    Project skeleton created

--- ex_1/src_to_implement/NeuralNetwork.py
+++ ex_1/src_to_implement/NeuralNetwork.py
@@ -3,6 +3,7 @@
 # Self imports
 from Optimization.Optimizers import Sgd
 # Other imports
+import numpy as np
 
 class NeuralNetwork:
     def __init__(self, optimizer: Sgd) -> None:
@@ -12,5 +13,17 @@ class NeuralNetwork:
         self.data_layer = None
         self.loss_layer = None
 
-    def forward(self):
-        
+    def forward(self) -> np.ndarray:
+        pass
+
+    def backward(self) -> np.ndarray:
+        pass
+
+    def append_layer(self, layer) -> None:
+        pass
+
+    def train(self, iterations: int) -> None:
+        pass
+
+    def test(self, input_tensor: np.ndarray) -> None:
+        pass
