8de29a2663bc80f4b743400b60e68ea77264862d
sujitdebnath <sujit.debnath.bd@gmail.com>
2022-11-23 18:20:49 +0100

    Implemented ReLU class

--- ex_1/src_to_implement/Layers/ReLU.py
+++ ex_1/src_to_implement/Layers/ReLU.py
@@ -10,9 +10,16 @@ import numpy as np
 class ReLU(BaseLayer):
     def __init__(self) -> None:
         super().__init__()
+        self.cache = None # for storing the output of ReLU, which will be needed to compute backward propagation later
 
     def forward(self, input_tensor: np.ndarray) -> np.ndarray:
-        return np.maximum(0, input_tensor)
+        relu_output = np.maximum(0, input_tensor)   # ReLU Activation Function
+        self.cache  = np.copy(relu_output)          # stored the output of ReLU for backward propagation
+
+        return relu_output
 
     def backward(self, error_tensor: np.ndarray) -> np.ndarray:
-        pass
+        relu_derivative = np.where(self.cache<=0, 0, 1)
+        gradient        = error_tensor * relu_derivative # gradient of the cost with respect to output of ReLU
+        
+        return gradient
