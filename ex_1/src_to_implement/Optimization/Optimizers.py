9a70983b777362abe729917bffba37b4f85209f9
root <root@sandwichkernel.localdomain>
2022-11-25 14:39:05 +0100

    Format Changed

--- ex_1/src_to_implement/Optimization/Optimizers.py
+++ ex_1/src_to_implement/Optimization/Optimizers.py
@@ -10,7 +10,5 @@ class Sgd:
     def __init__(self, learning_rate: float) -> None:
         self.learning_rate = learning_rate
 
-    def calculate_update(
-        self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray
-    ) -> np.ndarray:
+    def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
         return weight_tensor - (self.learning_rate * gradient_tensor)
