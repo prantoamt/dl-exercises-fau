c64b8e469b6c866dc49e71b683c69f6c862bbefc
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2022-12-11 15:14:21 +0100

    Flatten skeliton added

---
+++ ex_2/src_to_implement/Layers/Flatten.py
@@ -0,0 +1,17 @@
+# Python imports
+
+# Third party imports
+import numpy as np
+
+# Self imports
+
+
+class Flatten:
+    def __init__(self) -> None:
+        pass
+
+    def forward(self, input_tensor: np.ndarray) -> np.ndarray:
+        pass
+
+    def backward(self, error_tensor: np.ndarray) -> np.ndarray:
+        pass
