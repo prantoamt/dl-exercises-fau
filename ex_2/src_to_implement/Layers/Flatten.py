ecef1baefeff28dbcee097f0775028f5bc4c7f20
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2022-12-11 18:57:21 +0100

    flatten tensor comment added

--- ex_2/src_to_implement/Layers/Flatten.py
+++ ex_2/src_to_implement/Layers/Flatten.py
@@ -4,14 +4,42 @@
 import numpy as np
 
 # Self imports
+from Layers.Base import BaseLayer
+from Layers.Initializers import Initializer
 
 
-class Flatten:
+class Flatten(BaseLayer):
     def __init__(self) -> None:
-        pass
+        self.trainable = False
 
     def forward(self, input_tensor: np.ndarray) -> np.ndarray:
-        pass
+        """
+        @Params:
+            input_tensor -> np.ndarray
+        returns:
+            flatten_tensor-> np.ndarray
+        Flattens the input tenson to be compatible with fully connected layes.
+        The input tensor in this layer is expected to be 4D.
+        For example: (9,3,4,11), where 9 is batch size.
+        Keeping the batch size same, the function flatten the 4D array and
+        returns the result.
+        """
+        self.input_tensor = input_tensor
+        return input_tensor.reshape(len(input_tensor), -1)
 
     def backward(self, error_tensor: np.ndarray) -> np.ndarray:
-        pass
+        """
+        @Params:
+            error_tensor(flattened) -> np.ndarray
+        returns:
+            un_flatten_tensor-> np.ndarray
+        Error is the a flatten tensor comming back from densed or fully connected
+        layers. The function reshapes the error tensor to the previous shape it
+        was to be compatible with convolution layers.
+        """
+        return error_tensor.reshape(self.input_tensor.shape)
+
+    def initialize(
+        weight_initializer: Initializer, bias_initializer: Initializer
+    ) -> tuple:
+        return super().initialize(bias_initializer)
