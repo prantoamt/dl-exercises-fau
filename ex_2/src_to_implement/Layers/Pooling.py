f8314056320e5589238b91df408fa0990fe683dd
root <root@sandwichkernel.localdomain>
2022-12-15 17:59:35 +0100

    Modified the Pooling layer

--- ex_2/src_to_implement/Layers/Pooling.py
+++ ex_2/src_to_implement/Layers/Pooling.py
@@ -13,65 +13,77 @@ class Pooling(BaseLayer):
         super().__init__()
         self.stride_shape = stride_shape
         self.pooling_shape = pooling_shape
+        self.input_tensor = None
+        self.output_tensor = None
+        self.output_tensor_max_ind_list = []
 
     def forward(self, input_tensor: np.ndarray) -> np.ndarray:
-        print(f"Input Tensor Shape: {input_tensor.shape}")
-        print(f"Stride Shape: {self.stride_shape}")
-        print(f"Pooling Shape: {self.pooling_shape}")
-        stride_horiz, stride_vert = self.stride_shape
-        pooling_shape_horiz, pooling_shape_vert = self.pooling_shape
+        self.input_tensor = input_tensor
+
+        stride_row, stride_col = self.stride_shape
+        pooling_shape_row, pooling_shape_col = self.pooling_shape
         batch_size, channel, image_row, image_col = input_tensor.shape
 
-        print("*"*100)
+        pool_row = (image_row-pooling_shape_row) // stride_row + 1
+        pool_col = (image_col-pooling_shape_col) // stride_col + 1
+        self.output_tensor = np.zeros((batch_size, channel, pool_row, pool_col))
 
-        output_tensor = np.array([batch_size, channel])
         for batch_ind in range(batch_size):
             image_3d = input_tensor[batch_ind]
-            print(f"3D Image Shape: {image_3d.shape}")
 
-            max_pooling_3d_img = np.array([])
             for channel_ind in range(channel):
                 image_2d = image_3d[channel_ind]
-                print(f"2D Image Shape: {image_2d.shape}")
-                print(image_2d)
-                max_pooling_2d_img = []
 
-                for start_row in range(0, image_row, stride_vert):
-                    end_row = start_row+pooling_shape_vert
+                max_pooling_2d_img = []
+                qaurter_ind_row, qaurter_ind_col = 0, 0
+                
+                for start_row in range(0, image_row, stride_row):
+                    end_row = start_row+pooling_shape_row
                     if end_row > image_row:
                         continue
                     
-                    for start_col in range(0, image_col, stride_horiz):
-                        end_col = start_col+pooling_shape_horiz
+                    for start_col in range(0, image_col, stride_col):
+                        end_col = start_col+pooling_shape_col
                         if end_col > image_col:
                             continue
-                        # print(image_2d[start_row:end_row, start_col:end_col], np.amax(image_2d[start_row:end_row, start_col:end_col]))
-                        max_pooling_2d_img.append(np.amax(image_2d[start_row:end_row, start_col:end_col]))
+                        
+                        image_sliced = image_2d[start_row:end_row, start_col:end_col]
+                        max_pool_value = np.amax(image_sliced)
+                        max_pooling_2d_img.append(max_pool_value)
+
+                        row_index, col_index = np.where(image_sliced == max_pool_value)
+                        max_pool_index = (row_index[0]+qaurter_ind_row, col_index[0]+qaurter_ind_col)
+                        self.output_tensor_max_ind_list.append(max_pool_index)
+                        qaurter_ind_col += stride_col
+
+                    qaurter_ind_col = 0
+                    qaurter_ind_row += stride_row
                 
-                pool_row = (image_row-pooling_shape_vert) // stride_vert + 1
-                pool_col = (image_col-pooling_shape_horiz) // stride_horiz + 1
                 max_pooling_2d_img = np.array(max_pooling_2d_img).reshape(pool_row, pool_col)
-                print(max_pooling_2d_img)
-                max_pooling_3d_img = np.append(max_pooling_3d_img, max_pooling_2d_img)
-                print(max_pooling_3d_img.shape)
-                # start_row, start_col = 0, 0
-                # end_row, end_col = start_row+pooling_shape_vert, start_col+pooling_shape_horiz
-                # print(image_2d[start_row:end_row, start_col:end_col])
-
-                # start_col = start_col+stride_horiz
-                # end_col = start_col+pooling_shape_horiz
-                # print(image_2d[start_row:end_row, start_col:end_col])
-
-                # start_row, start_col = start_row+stride_vert, 0
-                # end_row, end_col = start_row+pooling_shape_vert, start_col+pooling_shape_horiz
-                # print(image_2d[start_row:end_row, start_col:end_col])
-
-                # start_col = start_col+stride_horiz
-                # end_col = start_col+pooling_shape_horiz
-                # print(image_2d[start_row:end_row, start_col:end_col])
-        print(output_tensor.shape)
-        print("*"*100)
+                self.output_tensor[batch_ind, channel_ind, :, :] = max_pooling_2d_img
+        
+        return self.output_tensor
 
 
     def backward(self, error_tensor: np.ndarray) -> np.ndarray:
-        pass
+        output_error_tensor = np.zeros(self.input_tensor.shape)
+        batch_size, channel, img_err_row, img_err_col = error_tensor.shape
+        output_tensor_max_ind_counter = 0
+
+        for batch_ind in range(batch_size):
+            image_err_3d = error_tensor[batch_ind]
+
+            for channel_ind in range(channel):
+                image_err_2d = image_err_3d[channel_ind]
+
+                for img_err_rows in image_err_2d:
+                    for img_err_val in img_err_rows:
+                        target_row, target_col = self.output_tensor_max_ind_list[output_tensor_max_ind_counter]
+
+                        if output_error_tensor[batch_ind, channel_ind, target_row, target_col] == 0:
+                            output_error_tensor[batch_ind, channel_ind, target_row, target_col] = img_err_val
+                        else:
+                            output_error_tensor[batch_ind, channel_ind, target_row, target_col] += img_err_val
+                        output_tensor_max_ind_counter += 1
+        
+        return output_error_tensor
